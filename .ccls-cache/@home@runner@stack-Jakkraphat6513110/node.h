1017aaa48101c8a5f51ac83ae0991a361d684bcf
Jakkraphat65131 <123150808+Jakkraphat6513110@users.noreply.github.com>
2023-02-17 10:43:55 +0000

    complete

---
+++ .ccls-cache/@home@runner@stack-Jakkraphat6513110/node.h
@@ -0,0 +1,38 @@
+//
+//  linkedlist.h
+//  LinkedList_simple
+//
+//  Created by Mingmanas Sivaraksa
+//
+
+#ifndef linkedlist_h
+#define linkedlist_h
+struct node
+{
+    char data;
+    struct node *nextPtr;
+};
+
+typedef struct node Node;
+typedef struct node* NodePtr;
+
+// void push(NodePtr *top, int x){
+//     Node *new_node = (NodePtr)malloc(sizeof(Node));
+//     if(new_node){
+//         new_node->data = x;
+//         new_node->nextPtr = *top;
+//         *top = new_node;
+//     }
+// };
+
+// int pop(NodePtr *top){
+//     NodePtr t = *top;
+//     if(t){
+//         int value=t->data;
+//         t= *top;
+//         *top=t->nextPtr;
+//         free(t);
+//         return value;
+//     }
+// };
+#endif
